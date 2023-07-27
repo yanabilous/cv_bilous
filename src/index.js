c5c707c10b577de1a3858cd578ed7e38d121579b
Taras Petrichuk <t.petriichuk@gmail.com>
2023-07-27 08:40:47 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-      <BrowserRouter basename='/cv_bilous/'>
+      <HashRouter basename='/cv_bilous/'>
           <App />
-      </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>
 );
 
